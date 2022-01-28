82ee6319d91093a60a3bc055b67852e84d487a0b
li195111 <green07111@gmail.com>
2022-01-28 12:35:17 +0800

    Update

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 import os
+import json
 import discord
 from dotenv import load_dotenv
 
 from neuralintents import GenericAssistant
 
-chatbot = GenericAssistant()
+with open('intents.json','r',encoding='utf-8') as fp:
+    data = json.loads(fp.read())
+
+with open('intents.json','w',encoding='utf-8') as fp:
+    fp.write(json.dumps(data,indent=2))
+
+chatbot = GenericAssistant('intents.json')
+# chatbot.train_model()
+# chatbot.save_model()
+chatbot.load_model()
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('目前登入身份：', client.user)
+        
+    async def on_message(self, message:discord.Message):
+        if message.author == client.user:
+            return
+        print (message.content, message.channel)
+        response = chatbot.request(message.content)
+        await message.channel.send(response)
 
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 
-client = discord.Client()
+print (f"Bot running ...")
 
-async def on_message(message:discord.Message):
-    if message.author == client.user:
-        return
-    
-    if str(message.content).startswith('$aibot'):
-        pass
-    
+client = MyClient()
 
 client.run(TOKEN)
